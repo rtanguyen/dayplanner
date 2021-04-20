514219c34f1739ecfc5f22db68fc1d0cb9428b20
rtanguyen <rtanguyen@gmail.com>
2021-04-20 18:35:38 -0500

    getstorage finally

--- assets/script.js
+++ assets/script.js
@@ -2,7 +2,12 @@ var tasks = [];
 var eventText = ""
 var taskIndex = ""
 var eventTime = ""
-
+var storedTasks = []
+var rowEl = ""
+// $('"#"+ selectedTaskRow')
+var textAreaEl = ""
+var selectedTaskRow =""
+var selectedTaskText = ""
 
 //current date at top of calendar
 $("#currentDay").html(function () {
@@ -10,19 +15,17 @@ $("#currentDay").html(function () {
 });
 
 
-//create task
-$(".row").on("blur", "textarea", function () {
-    eventText = $(this).val().trim();
+//push tasks to array
+$(".row").on("click", "a", function (){
+    // console.log("parent", $(".row"));
+    // console.log("clicktop", $(this));
+    //turn into variables
+    //save to local storage with parent id as key
+    //save input value 
+    eventText = $(this).siblings("textarea").val().trim();
     console.log(eventText)
     taskIndex = $(this).closest(".row").index();
     eventTime = $(this).closest(".row").attr("id");
-    // console.log(index)
-    // console.log(eventTime)
-});
-
-
-//push tasks to array
-$(".row").on("click", ".saveBtn", function (){
     //check for task update. if updating, replace event of index in array
     // console.log(taskIndex)
     if (typeof tasks[taskIndex] === 'undefined') {
@@ -44,13 +47,20 @@ var saveTasks = function () {
 
 //retrieve tasks from localstorage
 var loadTasks = function () {
-    tasks = JSON.parse(localStorage.getItem("savedTasks"));
-    console.log(tasks)
-    $.each(tasks, function() {
-        $("#tasks[taskIndex].time").children("textarea").html(tasks[taskIndex].event);
-    })
-};
+    storedTasks = JSON.parse(localStorage.getItem("savedTasks"));
 
+    if(!storedTasks) {
+        tasks = []
+    }
+
+    for (var i = 0; i < storedTasks.length; i++) {
+        selectedTaskRow = storedTasks[i].time
+        selectedTaskText = storedTasks[i].event
+        rowEl = $('#' + selectedTaskRow);
+        $(rowEl).children("textarea").html(function () {
+        return selectedTaskText})}
+        console.log(rowEl)
+   }
 
 
 // load tasks for the first time
