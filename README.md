eb775a8eb92c8a1f48a189cce6858527effc920f
rtanguyen <rtanguyen@gmail.com>
2021-04-23 14:07:47 -0500

    local storage and readme

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Work Day Scheduler Starter Code
+
+Link to deployed application: https://rtanguyen.github.io/dayplanner/
+
+![Planner](assets/images/planner.jpg)
+
+## User Story
+
+![User Story](assets/images/user-story.jpg)
+
+## Acceptance Criteria
+
+![Acceptance Criteria](assets/images/acceptance-criteria.jpg)
--- assets/script.js
+++ assets/script.js
@@ -1,91 +1,89 @@
-var tasks = [];
-var eventText = ""
-var taskIndex = ""
-var eventTime = ""
-var storedTasks = []
-var rowEl = ""
+var tasks = new Array(9).fill("");
+var eventText = "";
+var taskIndex = "";
+var eventTime = "";
+var storedTasks = [];
+var rowEl = "";
 // $('"#"+ selectedTaskRow')
-var textAreaEl = ""
-var selectedTaskRow =""
-var selectedTaskText = ""
-var currentTime = parseInt(moment().format("k"))
-var time = ""
+var textAreaEl = "";
+var selectedTaskRow = "";
+var selectedTaskText = "";
+var currentTime = parseInt(moment().format("k"));
+var time = "";
 
 //current date at top of calendar
 $("#currentDay").html(function () {
-    return moment().format("dddd, MMMM Do")
+  return moment().format("dddd, MMMM Do");
 });
 
-
 //push tasks to array
-$(".row").on("click", "a", function (){
-    eventText = $(this).siblings("textarea").val().trim();
-    console.log(eventText)
-    taskIndex = $(this).closest(".row").index();
-    eventTime = $(this).closest(".row").attr("id");
-    //check for task update. if updating, replace event of index in array
-    // console.log(taskIndex)
-    if (typeof tasks[taskIndex] === 'undefined') {
-        tasks.push({
-            event: eventText,
-            time: eventTime
-        });
-    } else {
-        tasks[taskIndex].event = eventText
-    }
-    saveTasks();
-    console.log(tasks);
+$(".row").on("click", "a", function () {
+  eventText = $(this).siblings("textarea").val().trim();
+  console.log(eventText);
+  taskIndex = $(this).closest(".row").index();
+  //   console.log(taskIndex);
+  eventTime = $(this).closest(".row").attr("id");
+  //check for task update. if updating, replace event of index in array
+  // console.log(taskIndex)
+  tasks[taskIndex] = {
+    event: eventText,
+    time: eventTime,
+  };
+  saveTasks();
+  console.log(tasks);
 });
 
 //save tasks in localstorage
 var saveTasks = function () {
-    localStorage.setItem("savedTasks", JSON.stringify(tasks));
-}
+  localStorage.setItem("savedTasks", JSON.stringify(tasks));
+};
 
 //retrieve tasks from localstorage
 var loadTasks = function () {
-    tasks = JSON.parse(localStorage.getItem("savedTasks"));
-
-    if(!tasks) {
-        tasks = []
-    } else {
+  tasks = JSON.parse(localStorage.getItem("savedTasks"));
+  if (!tasks) {
+    tasks = new Array(9).fill("");
+  } else {
     for (var i = 0; i < tasks.length; i++) {
-        selectedTaskRow = tasks[i].time
-        selectedTaskText = tasks[i].event
-        rowEl = $('#' + selectedTaskRow);
-        $(rowEl).children("textarea").html(function () {
-        return selectedTaskText})}
-        // console.log(rowEl)
-   }
-}
-
+      selectedTaskRow = tasks[i].time;
+      selectedTaskText = tasks[i].event;
+      rowEl = $("#" + selectedTaskRow);
+      $(rowEl)
+        .children("textarea")
+        .html(function () {
+          return selectedTaskText;
+        });
+    }
+    // console.log(rowEl)
+  }
+};
 
 setColor = function () {
-    $("textarea").each(function () {
-//reset background color at beginning of interval
-    $(this).removeClass("past present future")
+  $("textarea").each(function () {
+    //reset background color at beginning of interval
+    $(this).removeClass("past present future");
 
-//grab time from id
-    time = $(this).attr("id").replace("text-","")
-    var textId = "text-" + time
+    //grab time from id
+    time = $(this).attr("id").replace("text-", "");
+    var textId = "text-" + time;
     // console.log(textId);
     // console.log(currentTime)
 
-//conditional formatting based on time
-    if (time == currentTime) { 
-        $('#' + textId).addClass('present');
+    //conditional formatting based on time
+    if (time == currentTime) {
+      $("#" + textId).addClass("present");
     } else if (time > currentTime) {
-        $('#' + textId).addClass('future');
+      $("#" + textId).addClass("future");
     } else if (time < currentTime) {
-        $('#' + textId).addClass('past');
-}
-})
-}
+      $("#" + textId).addClass("past");
+    }
+  });
+};
 //change color every hour
-setInterval(function() {
-    setColor();
-    alert("hi");
-}, (1000 * 60) * 60);
+setInterval(function () {
+  setColor();
+  alert("hi");
+}, 1000 * 60 * 60);
 
 // load tasks for the first time
 loadTasks();
